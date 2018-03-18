1090e23ddb500cc0ee69a271fc38b30b8ca5298c
Afrizal Fikri <laser.survivor@gmail.com>
2018-03-18 20:03:57 +0700

    Multistream

--- pfac_kernel.cu
+++ pfac_kernel.cu
@@ -39,13 +39,14 @@
  *
  */
  
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cassert>
+#include <cctype>
+#include <cstdarg>
+
 #include <cuda_runtime.h>
-#include <assert.h>
-#include <ctype.h>
-#include <stdarg.h>
 
 #include "pfac.h"
 
@@ -108,7 +109,7 @@ __host__  PFAC_status_t  PFAC_kernel_timeDriven_wrapper(
     bool smem_on = ((4*EXTRA_SIZE_PER_TB-1) >= handle->maxPatternLen) ;
     
 #define PFAC_TEXTURE_ON 1
-    bool texture_on = (PFAC_TEXTURE_ON == handle->textureMode );
+    bool texture_on = (PFAC_TEXTURE_ON != handle->textureMode );
 
     PFAC_PRINTF("texture on = %d\n", texture_on);
 	
